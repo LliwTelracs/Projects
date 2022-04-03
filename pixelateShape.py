c4e4f02eb8b03b86ede9ad9c322aaddb8fddf2c1
Alexander Newcombe <ajnewcom@uwaterloo.ca>
2022-04-03 14:40:17 -0400

    Finished program to convert regular shapes into pixelated representations

--- pixelateShape.py
+++ pixelateShape.py
@@ -1,12 +1,26 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
-def pixelateShape(sideLen,sideCount,rotation):
+def pixelateShape(sideLen,sideCount,rotation=0):
     if(sideCount<3):
         return
-    corner=[(0,0)]
     sides=[]
-    internalAngle=(180
+    rotation=rotation*math.pi/180
+    angle=math.pi*2/sideCount
     posX=0
     posY=0
-    for i in range(1,sideCount):
-        posX=posX+cos(rotation+
+    for i in range(0,sideCount):
+        side=[(round(posX),round(posY))]
+        slope=(math.cos(rotation+angle*i),math.sin(rotation+angle*i))
+        for l in range(2*sideLen):
+            posX=posX+0.5*slope[0]
+            posY=posY+0.5*slope[1]
+            pt=(round(posX),round(posY))
+            if pt not in side:
+                side.append((round(posX),round(posY)))
+        sides.append(side)
+    return sides
+
+
